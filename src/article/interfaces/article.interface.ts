3dbf5c0ae9fde352065f97e6927da03374af2bd1
davmik2601 <89848236+davmik2601@users.noreply.github.com>
2022-05-24 21:02:35 +0400

    save changes

--- src/article/interfaces/article.interface.ts
+++ src/article/interfaces/article.interface.ts
@@ -1,9 +1,9 @@
-import {Document} from "mongoose"
+import { Document } from 'mongoose';
 
-export interface Article extends Document{
+export interface Article extends Document {
   readonly name: string;
   readonly description: string;
   readonly imageURL: string;
   readonly price: number;
   readonly createdAt: Date;
-};
+}
